53f64b17a13bdfb5417470133b216250bf1c4ece
Dan Reich <dkreich@gmail.com>
2016-03-04 03:55:00 -0800

    trying out a body parser

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
