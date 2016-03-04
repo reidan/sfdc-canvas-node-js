f00ce8c42da4b6b73cbd4daf4de14354d9dc3973
Dan Reich <dkreich@gmail.com>
2016-03-04 04:17:29 -0800

    simple setup

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use( bodyParser.raw( ) );
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.use(express.static(__dirname + '/public'));
 
