0e90f7cc522ac45d6aba86a6dc4adce0d7404f1c
Dan Reich <dkreich@gmail.com>
2016-03-04 03:03:47 -0800

    trying to handle the signed request

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var hmacSHA256 = require('crypto-js/hmac-sha256');
+var encBase64 = require('crypto-js/enc-base64');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -11,6 +13,31 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+app.post('/canvas', function(request, response)  {
+
+    // Receive the POST message that contains the initial signed request from Salesforce. DONE!
+    // Split the signed request on the first period. The result is two strings: the hashed Based64 context signed with the consumer secret and the Base64 encoded context itself.
+    console.log( request );
+    var index = request.indexOf( '.' );
+    console.log( index );
+    var hashedBase64 = request.substring(0, index);
+    var signedHashedBase64 = request.substring(index + 1, request.length());
+
+
+    //$calcedSig = base64_encode(hash_hmac("sha256", $encodedEnv, $consumer_secret, true));	
+    // Use the HMAC SHA-256 algorithm to hash the Base64 encoded context and sign it using your consumer secret.
+    var hash = hmacSHA256( signedHashedBase64, process.env.CONSUMER_SECRET );
+    // Base64 encode the string created in the previous step.
+    var encodedHash = encBase64.encode( hash );
+    // Compare the Base64 encoded string with the hashed Base64 context signed with the consumer secret you received in step 2.
+    if( encodedHash !== hashedBase64 ) {
+    	response.status( 500 );
+    } else {
+    	var signedRequest = encBase64.decode( signedHashedBase64 );
+  		response.send( signedRequest );
+    }
 });
 
 app.listen(app.get('port'), function() {
