edbbd71c3e8b6cfe6e7f35c53da398e78abb151e
Dan Reich <dkreich@gmail.com>
2016-03-04 03:58:20 -0800

    take 3

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
